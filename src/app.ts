182560bebfed87ea1a0d32a4869e0321a9564435
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-06-16 13:06:22 -0400

    Add about.html to src

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import {RouterConfiguration, Router} from 'aurelia-router';
 import { PLATFORM } from "aurelia-framework";
   
 export class App {
-
   configureRouter(config: RouterConfiguration, router: Router): void {
     config.title = 'Routing demo';
+    config.options.pushState = true;
     config.map([
-      { route: '',   moduleId: PLATFORM.moduleName('home/home'), title: 'Home' }
+      { route: '',   moduleId: PLATFORM.moduleName('home/home'), title: 'Home' },
+      { route: '/about',   moduleId: PLATFORM.moduleName('about/about.html'), title: 'About' }
     ]);
   }
 }
