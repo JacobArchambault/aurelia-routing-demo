f9bbb9177a612a138e4c3376aca75082f0643270
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-06-16 12:53:18 -0400

    Get home page to display with router configuration

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
-import { Home } from './home/home';
+import {RouterConfiguration, Router} from 'aurelia-router';
+import { PLATFORM } from "aurelia-framework";
+  
 export class App {
-  message = new Home().message;
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    config.title = 'Routing demo';
+    config.map([
+      { route: '',   moduleId: PLATFORM.moduleName('home/home'), title: 'Home' }
+    ]);
+  }
 }
